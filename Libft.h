0e6a52ad35d97f5d83121d52c00fd7e1a0a84b41
yd7w <yahyaboukhmira7@gmail.com>
2025-01-20 20:10:19 +0100

    more progress

--- Libft.h
+++ Libft.h
@@ -38,5 +38,7 @@ int ft_atoi(const char *str);
 char *ft_strnstr(const char *str, const char *needle, size_t n);
 void    *ft_calloc(size_t n, size_t nsize);
 char *ft_strdup(const char *str);
+char *ft_substr(char const *s, unsigned int start, size_t len);
+char *ft_strjoin(char const *s1, char const *s2);
 
 #endif
--- ft_strjoin.c
+++ ft_strjoin.c
@@ -1,36 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/01/19 16:33:52 by marvin            #+#    #+#             */
-/*   Updated: 2025/01/19 16:33:52 by marvin           ###   ########.fr       */
+/*   Created: 2025/01/20 18:07:05 by marvin            #+#    #+#             */
+/*   Updated: 2025/01/20 18:07:05 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Libft.h"
 
-char *ft_strdup(const char *str)
+char *ft_strjoin(char const *s1, char const *s2)
 {
-    size_t strleng;
-    size_t i;
     char *newstr;
+    size_t newstrlen;
 
-    if (!str)
+    if (!s1 && !s2)
         return (NULL);
-
-    i = 0;
-    strleng = ft_strlen(str);
-    newstr = (char *)malloc(strleng + 1);
+    if (!s1)
+        return (ft_strdup(s2));
+    if (!s2)
+        return (ft_strdup(s1));
+    newstrlen = ft_strlen(s1) + ft_strlen(s2) + 1;
+    newstr = (char *)malloc(newstrlen * sizeof(char));
     if (!newstr)
         return (NULL);
-    while (str[i] != '\0')
-    {
-        newstr[i] = str[i];
-        i++;
-    }
-    newstr[i] = '\0';
+    ft_strlcpy(newstr, s1, newstrlen);
+    ft_strlcat(newstr, s2, newstrlen);    
     return (newstr);
 }
