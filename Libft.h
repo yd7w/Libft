6e8e8415dca5327510b26ca5891f60b8bb07e9e2
yd7w <yahyaboukhmira7@gmail.com>
2025-01-29 18:12:44 +0100

    completed the normal part of libft

--- Libft.h
+++ Libft.h
@@ -49,5 +49,6 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void	ft_striteri(char *s, void (*f)(unsigned int, char*));
 void	ft_putchar_fd(char c, int fd);
 void	ft_putnbr_fd(int n, int fd);
+char	*ft_strchr(const char *str, int ch);
 
 #endif
